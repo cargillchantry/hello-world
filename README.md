bd78e2ac470ae663c2008b9c7348298aa72558b6
Chantry <chantry.cargill@skybettingandgaming.com>
2020-10-16 11:43:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-world
+
+Simple service with a /health endpoint for experimenting with CI/CD.
---
+++ src/main/kotlin/hello/world/Application.kt
@@ -0,0 +1,11 @@
+package hello.world
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("hello.world")
+		.start()
+}
+
